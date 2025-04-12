4532805c910b5aa59589b9c7e703b84fd54cb3f1
G00386269 <G00386269@atu.ie>
2025-04-12 16:51:19 +0100

    Added funtionality to StartPage - Created variables to store users data and display them correctly on screen

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule} from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    IonicModule,
+    RouterModule
+  ]
 })
 export class DashboardPage implements OnInit {
-
-  constructor() { }
+  name: string = '';
+  income: number = 0;
+  goal: number = 0;
+  spent: number = 0;
+  remaining: number = 0;
+  spentPercent: number = 0;
 
   ngOnInit() {
+    this.name = localStorage.getItem('name') || 'User';
+    this.income = Number(localStorage.getItem('income') || 0);
+    this.goal = Number(localStorage.getItem('goal') || 0);
+    this.spent = 0; // This will come from stored expenses later
+    this.remaining = this.income - this.goal;
+    this.spentPercent = ((this.spent / this.income) * 100).toFixed(2) as unknown as number;
   }
-
 }
