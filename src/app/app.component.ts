00f6c50dac673327aad1de9738b2dbaf9caf10cc
G00386269 <G00386269@atu.ie>
2025-04-15 01:36:37 +0100

    General CSS clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
+  template: '<ion-app><router-outlet></router-outlet></ion-app>',
   imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
+
+import { defineCustomElements } from '@ionic/core/loader';
+defineCustomElements(window);
+
+document.body.setAttribute('color-theme', 'dark');
 
 bootstrapApplication(AppComponent, {
   providers: [
