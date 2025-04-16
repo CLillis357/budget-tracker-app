83959a6f67351822d35faaf03e0ea545794c90b2
G00386269 <G00386269@atu.ie>
2025-04-16 15:17:38 +0100

    Edited Start Page so that when a user wants to edit their profile, the previously saved data will fill out the form.

--- src/app/pages/start/start.page.ts
+++ src/app/pages/start/start.page.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { Firestore, doc, setDoc } from '@angular/fire/firestore';
 import { Auth } from '@angular/fire/auth';
+import { getDoc } from '@angular/fire/firestore';
 import { inject } from '@angular/core';
 
 @Component({
@@ -14,7 +15,7 @@ import { inject } from '@angular/core';
   styleUrls: ['./start.page.scss'],
   imports: [CommonModule, FormsModule, IonicModule]
 })
-export class StartPage {
+export class StartPage implements OnInit {
   name: string = '';
   income: number = 0;
   goal: number = 0;
@@ -23,6 +24,13 @@ export class StartPage {
   auth: Auth = inject(Auth);
 
   constructor(private router: Router) {}
+
+  ngOnInit() {
+    const profile = JSON.parse(localStorage.getItem('profile') || '{}');
+    this.name = profile.name || '';
+    this.income = profile.income || 0;
+    this.goal = profile.goal || 0;
+  }
 
   async startCalculation() {
     if (!this.name || !this.income || !this.goal) {
@@ -40,12 +48,23 @@ export class StartPage {
     await setDoc(userDocRef, {
       name: this.name,
       income: this.income,
-      goal: this.goal,
-
+      goal: this.goal
     });
-    
 
+    const updatedSnapshot = await getDoc(userDocRef);
+    if (updatedSnapshot.exists()) {
+      localStorage.setItem('profile', JSON.stringify(updatedSnapshot.data()));
+    }
 
-    this.router.navigateByUrl('/dashboard');
+    const profile = {
+      name: this.name,
+      income: this.income,
+      goal: this.goal
+    };
+    localStorage.setItem('profile', JSON.stringify(profile));
+
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      window.location.href = '/dashboard';
+    });
   }
 }
